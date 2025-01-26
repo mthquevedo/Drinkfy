5f597070ee9d06b4e25829af34a598aa94fb3ab0
Matheus Quevedo <matheusq10@gmail.com>
2025-01-26 17:11:56 -0300

    Ajuste: página de utiliade atualizada

---
+++ produtos/src/utils.ts
@@ -0,0 +1,22 @@
+import { FaBottleWater } from "react-icons/fa6";
+import { PiBowlSteamFill } from "react-icons/pi";
+import { RiDrinks2Fill, RiDrinksFill } from "react-icons/ri";
+import { SiBuymeacoffee } from "react-icons/si";
+
+export const storeItens = [
+    { name: "Refrigerante", icon: RiDrinks2Fill },
+    { name: "Refrigerante", icon: RiDrinks2Fill },
+    { name: "Refrigerante", icon: RiDrinks2Fill },
+    { name: "Café", icon: SiBuymeacoffee },
+    { name: "Café", icon: SiBuymeacoffee },
+    { name: "Café", icon: SiBuymeacoffee },
+    { name: "Chá", icon: PiBowlSteamFill },
+    { name: "Chá", icon: PiBowlSteamFill },
+    { name: "Chá", icon: PiBowlSteamFill },
+    { name: "Suco", icon: RiDrinksFill },
+    { name: "Suco", icon: RiDrinksFill },
+    { name: "Suco", icon: RiDrinksFill },
+    { name: "Água", icon: FaBottleWater },
+    { name: "Água", icon: FaBottleWater },
+    { name: "Água", icon: FaBottleWater }
+]
