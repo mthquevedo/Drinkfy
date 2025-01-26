94cf2979f25f5c04cc8e943d481e35e12232f880
Matheus Quevedo <matheusq10@gmail.com>
2025-01-25 21:15:01 -0300

    Ajuste: limpeza de lógica e adição de novos estilos

--- login/src/App.tsx
+++ login/src/App.tsx
@@ -13,15 +13,12 @@ export default function App() {
     }, 1000)
   }, []);
 
-  const options: DateTimeFormatOptions = { day: "numeric", month: "long" };
-  const formattedDate = currentDate.toLocaleDateString("pt-BR", options);
   const hour: number = currentDate.getHours();
   const minutes: number = currentDate.getMinutes();
 
   return (
     <main
-      className="flex flex-col w-lvw h-lvh px-8 py-13 overflow-hidden justify-between"
-      style={{ backgroundColor: "var(--color-primary)" }}
+      className="font-(family-name:--font-roboto) flex flex-col bg-(--color-primary) w-lvw h-lvh px-8 py-13 overflow-hidden justify-between"
     >
       <div className="flex items-center justify-between text-white text-xl w-full">
         <img src={Logo} alt="Logo" className="w-26 h-auto" />
@@ -31,23 +28,29 @@ export default function App() {
       </div>
 
       <div className="flex flex-col">
-        <div className="flex items-center justify-center flex-auto bg-white overflow-hidden">
-          <img src={Banner} alt="Banner" className="w-26 h-auto" />
+        <div className="flex items-center justify-center flex-auto h-114 bg-white rounded-3xl overflow-hidden mb-8">
+          <img src={Banner} alt="Banner" className="w-full h-auto" />
         </div>
 
-        <h1
-          className="m-0 text-2xl font-bold"
-          style={{ color: "var(--color-greenhighlight)" }}
-        >
+        <h1 className="text-(--color-greenhighlight) m-0 text-2xl font-bold mb-3">
           Sua bebida na palma da mão!
         </h1>
 
-        <p>Com apenas alguns cliques peça sua bebida de
+        <p className="text-neutral-400">Com apenas alguns cliques peça sua bebida de
           qualquer parte do evento, nós iremos até você.</p>
       </div>
 
       <div>
+        <p className="text-sm text-white mb-7">Continuar como <strong>Matheus</strong> ou utilizar uma nova conta?</p>
 
+        <div className="flex items-center justify-between">
+          <button className="py-3 px-8 text-neutral-300 bg-neutral-400/20 border-2 border-neutral-100 rounded-full">
+            Cadastrar
+          </button>
+          <button className="py-3 px-19 text-(--color-primary) font-medium bg-(--color-greenhighlight) rounded-full">
+            Entrar
+          </button>
+        </div>
       </div>
 
 
